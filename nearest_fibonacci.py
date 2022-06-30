69c9a0248fcb489df02fddc87d0549a4013097e7
Harshithasaisri <96580368+Harshithasaisri@users.noreply.github.com>
2022-06-30 18:44:43 +0530

    n=int(input()) a=0 b=1 while(a<=n):     i=a     c=a+b     a=b     b=c     s=a x=abs(n-i) y=abs(n-s) if(x<y):    print(i) elif(y==x):    print(i,s) else:    print(s)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,29 +1,17 @@
-def is_fib(j):
-    a = 0
-    b = 1
-    c = a+b
-    while c<j:
-        c = a + b
-        a = b
-        b = c
-    if c == j:
-        return j
-
-
-n = int(input())
-k=n
-for j in range(n,0,-1):
-    if is_fib(j):
-        a = j
-        break
-while k!=0:
-    if is_fib(k):
-        b = k
-        break
-    k+=1
-if (n-a)<(b-n):
-    print(a)
-elif (n-a)==(b-n):
-    print(a,b)
+n=int(input())
+a=0
+b=1
+while(a<=n):
+    i=a
+    c=a+b
+    a=b
+    b=c
+    s=a
+x=abs(n-i)
+y=abs(n-s)
+if(x<y):
+   print(i)
+elif(y==x):
+   print(i,s)
 else:
-    print(b)
+   print(s)
