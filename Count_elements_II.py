e6fec1187f61c0142209d84e06ab4926ddd6383c
Harshithasaisri <96580368+Harshithasaisri@users.noreply.github.com>
2022-07-09 17:25:05 +0530

    n,m=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) a=[] s1=set(l1) s2=set(l2) #print(s1) #print(s2) c=0 for i in s1:     for j in s2:         if i==j:             c+=1 print(len(s1)+len(s2)-2*c)

---
+++ Count_elements_II.py
@@ -0,0 +1,14 @@
+n,m=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+a=[]
+s1=set(l1)
+s2=set(l2)
+#print(s1)
+#print(s2)
+c=0
+for i in s1:
+    for j in s2:
+        if i==j:
+            c+=1
+print(len(s1)+len(s2)-2*c)            
